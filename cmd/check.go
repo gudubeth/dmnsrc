f9bdccf2d094cfa46de11f2131a9430ebe720800
Ozgur Erdem <gudubeth@gmail.com>
2018-04-29 23:29:08 +0300

    Refactor check command code

--- cmd/check.go
+++ cmd/check.go
@@ -12,11 +12,18 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// checkCmd represents the check command
-var checkCmd = &cobra.Command{
-	Use:   "check",
-	Short: "Check the vailability of domains",
-	Long: `|
+type checkCmdFlags struct {
+	showWhois bool
+	benchmark bool
+}
+
+func NewCheckCommand() *cobra.Command {
+	var flags checkCmdFlags
+
+	var cmd = &cobra.Command{
+		Use:   "check",
+		Short: "Check the vailability of domains",
+		Long: `|
 Checks the availability of domains. It needs full domain names. If you want
 extended search with domain name generation, see the "search" command. 
 
@@ -25,23 +32,25 @@ For multiple domain checks, seperate domain names with comma or space
 Examples:
 	dmn check example.com
 	dmn check example.com example.org
-
 `,
-	Args: cobra.MinimumNArgs(1),
-	Run:  runCheckCmd,
-}
+		Args: cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			runCheckCmd(flags, args)
+		},
+	}
 
-var showWhois = false
-var benchmark = false
+	cmd.Flags().BoolVarP(&flags.showWhois, "whois", "w", false, "Show whois information of domain name")
+	cmd.Flags().BoolVarP(&flags.benchmark, "benchmark", "b", false, "Benchmark")
+
+	return cmd
+}
 
 func init() {
-	rootCmd.AddCommand(checkCmd)
-	checkCmd.Flags().BoolVarP(&showWhois, "whois", "w", false, "Show whois information of domain name")
-	checkCmd.Flags().BoolVarP(&benchmark, "benchmark", "b", false, "Benchmark")
+	rootCmd.AddCommand(NewCheckCommand())
 }
 
-func runCheckCmd(cmd *cobra.Command, args []string) {
-	if benchmark {
+func runCheckCmd(flags checkCmdFlags, args []string) {
+	if flags.benchmark {
 		defer dev.PrintElapsedTime("Running time", time.Now())
 	}
 	allNames := []string{}
@@ -54,7 +63,7 @@ func runCheckCmd(cmd *cobra.Command, args []string) {
 
 	for record := range out {
 		benchmarkStr := ""
-		if benchmark {
+		if flags.benchmark {
 			benchmarkStr = fmt.Sprintf(" (%.3f)", float32(record.Elapsed/time.Millisecond)/1000)
 		}
 
@@ -66,7 +75,7 @@ func runCheckCmd(cmd *cobra.Command, args []string) {
 			color.Yellow("✘ %s: unavailable%s", record.Name, benchmarkStr)
 		}
 
-		if record.Error == nil && showWhois {
+		if record.Error == nil && flags.showWhois {
 			fmt.Println(strings.Repeat("=", 80))
 			fmt.Println(record.Response)
 			fmt.Println(strings.Repeat("=", 80))
